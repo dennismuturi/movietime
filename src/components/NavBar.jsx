f0fc3dbc1490a26125120d4e14fa85e76025ab84
dennismuturi <dennismuturi1@gmail.com>
2023-01-15 19:32:42 +0300

    resolve Home page link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,13 +11,13 @@ export default function NavBar(){
 <div className="container mx-auto px-6 py-2 flex justify-between items-center">
     
     <div className="flex p-3 ">
-        <a to="/favorites"  data-baseweb="link" 
-        href="https://movietime-xi.vercel.app/" 
+        <Link to="/"  data-baseweb="link" 
         target="_self" 
         aria-label="Go to Movie Time" 
         data-tracking-name=""
-        className=" mr-4 cc cd ce cf cg ch ci cj bn b5 ck cl cm cn co cp cq cr cs ct cu cv cw bt bf cb cx cy cz d0">Movie Time
-       </a>
+        className=" mr-4 cc cd ce cf cg ch ci cj bn b5 ck cl cm cn co cp cq cr cs ct cu cv cw bt bf cb cx cy cz d0">
+            Movie Time
+       </Link>
  </div>
 
         <div className="flex p-3">
